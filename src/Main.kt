5ad2486a40fdb9cbf86ff3dc2487d54d409bcf90
Kevin-Kip <masterfork5@gmail.com>
2018-10-05 08:47:01 +0300

    Comments on the code and more code

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,15 @@
 
 fun main(args: Array<String>){
+    //creating an instance of a class
     val p = Person("","")
-    p.name = "Kevin"
-    p.email = "kevin@gmail.com"
+    p.name = "Kevin" //setter
+    p.email = "kevin@gmail.com" // setter
 
-    println("The name is ${p.name}")
+    println("The name is ${p.name}") // getter
 
-    val x = 2
+    val x = 2 // variable declaration. This can be val x: Int = 2
+
+    // This is the equivqlent if a switch statement
     when(x){
         1 -> println("X is 1")
         2 -> println("X is 2")
@@ -15,10 +18,36 @@ fun main(args: Array<String>){
         }
     }
 
+    // for loop
+    /*
+    * This is like:
+    *
+    * // Java
+    * for (int i = 1; i <= 10; i+2){
+    *   System.out.println(i)
+    * }
+    *
+    *
+    * */
     for (i in 1..10 step 2){
         println(i)
     }
 
+    // simplified if statement
+    /*
+    * This is like:
+    *
+    * //kotlin
+    * if(x == 2){
+    *   println("Hooray")
+    * } else {
+    *   println("Not a 2")
+    * }
+    * */
     val y = if (x == 2) "Hooray" else "Not a 2"
     println(y)
+
+    //list
+    val myList = listOf(1,2,3,4,5,"3")
+    for (item in myList) println("Value of item is ${item}")
 }
--- src/Person.kt
+++ src/Person.kt
@@ -1,1 +1,3 @@
+// This is the equivalent of the POJO class in Java
+// NOTE: we don't need to write getters and setters here
 data class Person(var name: String, var email: String)
